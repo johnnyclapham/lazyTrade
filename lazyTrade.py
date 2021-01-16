f3303b21f3a2e457a62db405d48339d8ff0d064c
johnnyclapham <jmclapham@email.wm.edu>
2021-01-16 16:28:00 -0500

    basic bot working

--- lazyTrade.py
+++ lazyTrade.py
@@ -1,4 +1,30 @@
-#first
-print("hello world!")
+import discord
 
-print("\ngoodbye!")
+'''
+Simple bare-bones discord bot for further development.
+Created for Python 3.7+
+@author johnnyclapham 2021
+'''
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Bot with userid: {0.user} connected'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello! how are you?')
+    if message.content.startswith('good'):
+        await message.channel.send('Really? That is excellent news.')
+    if message.content.startswith('bye'):
+        await message.channel.send('until next time!')
+    if message.content.startswith('bring me news'):
+        await message.channel.send('until next time!')
+
+
+client.run('ODAwMDkzMzM4NTA1NzA3NTcw.YANG_A.26t_XApvJ-9kCndTIZPYSuW_LIY')
